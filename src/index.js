595cbc93fa8acc251646230c296873e10710df91
Londeka2 <163391242+Londeka01@users.noreply.github.com>
2024-03-24 14:58:38 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = Math.round(temperature);
   temperatureElement.innerHTML = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+
   cityElement.innerHTML = response.data.city;
+  descriptionElent.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
 function searchCity(city) {
   let apiKey = "bb54ef58483ffo06a270b069935bd6t4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-let cityElement = document.querySelector("#city");
+let cityElemnt = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
-
-searchCity("Lisbon");
